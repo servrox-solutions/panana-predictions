724f9c690e8397747fd955a3d792055d53c1a7ea
Marcel Mayer <mm@servrox.solutions>
2024-09-11 09:20:44 +0200

    ⚗️ test(frontend): link to test page

--- components/header.tsx
+++ components/header.tsx
@@ -114,12 +114,12 @@ export function Header() {
         </BreadcrumbList>
       </Breadcrumb>
       <div className="relative ml-auto flex-1 md:grow-0">
-        <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
+        {/* <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
         <Input
           type="search"
           placeholder="Search..."
           className="w-full rounded-lg bg-background pl-8 md:w-[200px] lg:w-[336px]"
-        />
+        /> */}
       </div>
       <WalletSelector />
       <ModeToggle />
