32495aa92fc4f69fedb2ff4376757c936ec42e11
Marcel Mayer <mm@servrox.solutions>
2024-09-16 21:13:47 +0200

    🚧 wip

--- app/(dashboard)/markets/page.tsx
+++ app/(dashboard)/markets/page.tsx
@@ -1,6 +1,9 @@
 import MarketOrganizer from "@/components/market-organizer";
+import { surfClient } from "@/lib/aptos";
 
 export default function Markets() {
+  // const [balance] = await surfClient.resource.
+
   return (
     <div className="pl-4">
       <MarketOrganizer />
