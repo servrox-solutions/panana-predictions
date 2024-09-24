294b9588206ef2647e1744611ff4dc106ab08f41
Aaron Roehl <ar@servrox.solutions>
2024-09-24 10:45:21 +0200

    feat(headers): add mizu proxy to allow cors header

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   serverExternalPackages: ["grammy", "@grammyjs/menu"],
+  async headers() {
+    return [
+      {
+        source: "/mizuwallet-connect-manifest.json",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin", value: "https://proxy.mz.xyz"
+          },
+        ],
+      },
+    ]
+  },
 };
 
 export default nextConfig;
