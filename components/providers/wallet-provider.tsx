478e3ffb35cd9dbbb59ea8f71fd70d8a6c1ce804
Marcel Mayer <mm@servrox.solutions>
2024-09-24 09:21:19 +0200

    ⚗️ test(frontend): mizu wallet

--- components/providers/wallet-provider.tsx
+++ components/providers/wallet-provider.tsx
@@ -13,13 +13,15 @@ export const WalletProvider = ({ children }: PropsWithChildren) => {
 
   return (
     <AptosWalletAdapterProvider
+      optInWallets={["Mizu Wallet", "Continue with Google"]}
       autoConnect={autoConnect}
       dappConfig={{
         network: (process.env.NEXT_PUBLIC_APP_NETWORK ?? "testnet") as Network,
         aptosApiKey: process.env.NEXT_PUBLIC_APTOS_API_KEY,
         aptosConnect: { dappId: "f44746dc-fd46-4765-a37c-f1b61fee51fa" },
         mizuwallet: {
-          manifestURL: "https://panana-predictions.xyz/manifest.json",
+          manifestURL:
+            "https://panana-predictions.xyz/mizuwallet-connect-manifest.json",
         },
       }}
       onError={(error) => notifyError(error || "Unknown wallet error")}
