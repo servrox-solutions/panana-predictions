e1b56e47ea392be679abf14e12415e9318448ca8
Marcel Mayer <mm@servrox.solutions>
2024-09-25 15:33:11 +0200

    ♻️ feat(frontend): auto cols navigation bottom

--- components/navigation-bar.tsx
+++ components/navigation-bar.tsx
@@ -1,5 +1,4 @@
 import { SidenavItem } from "./sidenav/sidenav-items";
-import { cn } from "@/lib/utils";
 import { headers } from "next/headers";
 import { NavigationBarItem } from "./navigation-bar-item";
 import { Home, ChartCandlestick, User } from "lucide-react";
@@ -29,16 +28,7 @@ export function NavigationBar() {
 
   return (
     <div className="z-50 w-full bg-white/30 backdrop-blur-lg border-t border-gray-200 dark:bg-gray-700/30 dark:border-gray-600 py-2 [--base-pb:0.5rem] pb-safe-combined transition-all max-h-16 min-h-16 sm:hidden">
-      <div
-        className={cn(
-          "grid h-full max-w-lg mx-auto font-medium",
-          items.length === 6 && "grid-cols-6",
-          items.length === 5 && "grid-cols-5",
-          items.length === 4 && "grid-cols-4",
-          items.length === 3 && "grid-cols-3",
-          items.length === 2 && "grid-cols-2"
-        )}
-      >
+      <div className="grid grid-flow-col auto-cols-fr h-full max-w-lg mx-auto font-medium">
         {items.map((item) => (
           <NavigationBarItem
             key={item.name}
