e8801dee7ad04f7361e0a6112243d7533f964e74
Marcel Mayer <mm@servrox.solutions>
2024-09-30 13:47:14 +0200

    🔒️ feat(frontend): enable basic auth expect for telegram webhook

--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,17 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
-// import { nextBasicAuthMiddleware } from "nextjs-basic-auth-middleware";
+import { nextBasicAuthMiddleware } from "nextjs-basic-auth-middleware";
 
 export function middleware(request: NextRequest) {
-  // const response = nextBasicAuthMiddleware(undefined, request);
-  // if (response.status === 401) return response;
+  const path = request.nextUrl.pathname;
+
+  if (path !== "/api/telegram/webhook") {
+    const response = nextBasicAuthMiddleware(undefined, request);
+    if (response.status === 401) return response;
+  }
 
   const headers = new Headers(request.headers);
-  headers.set("x-current-path", request.nextUrl.pathname);
+  headers.set("x-current-path", path);
   return NextResponse.next({ headers });
 }
 
