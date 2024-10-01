d3c7542134607542d3e6594fad0e8e160f11f750
Aaron Roehl <ar@servrox.solutions>
2024-10-01 15:06:10 +0200

    feat(market-title): set human readable title

--- components/market-title.tsx
+++ components/market-title.tsx
@@ -1,5 +1,5 @@
 import { cn } from "@/lib/utils";
-import { DateTime } from "luxon";
+import { DateTime, Duration } from "luxon";
 
 export interface MarketTitleProps {
   tradingPair: {
@@ -19,17 +19,28 @@ export const MarketTitle = ({
   as: Tag = "h2",
   className,
 }: MarketTitleProps) => {
+  const diff = DateTime.fromSeconds(resolveTime).diff(DateTime.fromSeconds(betCloseTime));
+
+  // Got function from here: https://github.com/moment/luxon/issues/1134
+  function toHuman(dur: Duration, smallestUnit = "seconds"): string {
+    const units = ["years", "months", "days", "hours", "minutes", "seconds", "milliseconds",];
+    const smallestIdx = units.indexOf(smallestUnit);
+    const entries = Object.entries(
+      dur.shiftTo(...(units as any)).normalize().toObject()
+    ).filter(([_unit, amount], idx) => amount > 0 && idx <= smallestIdx);
+    const dur2 = Duration.fromObject(
+      entries.length === 0 ? { [smallestUnit]: 0 } : Object.fromEntries(entries)
+    );
+    return dur2.reconfigure({ locale: 'en-GB' }).toHuman();
+  }
+
   return (
     <Tag className={cn("text-lg font-semibold", className)}>
       Will
       <span className="text-secondary bg-primary p-1 rounded mx-1">
         {tradingPair.one}/{tradingPair.two}
       </span>
-      go up or down within{" "}
-      {DateTime.fromSeconds(resolveTime)
-        .diff(DateTime.fromSeconds(betCloseTime))
-        .toFormat("hh:mm:ss")}{" "}
-      hours?
+      go up or down within {toHuman(diff)}?
     </Tag>
   );
 };
