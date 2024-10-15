44f114a632c47f907a84dc02c66f5299d92e19e3
Aaron Roehl <ar@servrox.solutions>
2024-10-15 15:20:04 +0200

    feat(profile): added event markets to profile stats

--- lib/nodit/operation-docs.ts
+++ lib/nodit/operation-docs.ts
@@ -1,37 +1,44 @@
 import { Address } from '../types/market';
 
 export const ProfilePageStatsQuery = (sender: Address, moduleAddress: Address) => `
-  query StatisticsPageQuery {
+query StatisticsPageQuery {
         created_markets: account_transactions_aggregate(
-            where: {account_address: {_eq: "${sender}"}, user_transaction: {entry_function_id_str: {_eq: "${moduleAddress}::market::create_market"}}}
+            where: {account_address: {_eq: "0x59e9982aaa5194058e51c8be75519ec54c518ea3651b3a4a87c39b9bd88ba314"}, user_transaction: {entry_function_id_str: {_in: ["0x6e042405369c44a5fec8ddd28bb47442cf0256367b74b9aea64ae0e6ed90b489::market::create_market", "0x6e042405369c44a5fec8ddd28bb47442cf0256367b74b9aea64ae0e6ed90b489::event_market::create_market"]}}}
         ) {
             aggregate {
             count
             }
         }
         market_interactions: account_transactions_aggregate(
-            where: {account_address: {_eq: "${sender}"}, user_transaction: {entry_function_id_str: {_like: "${moduleAddress}::market%"}}}
+            where: {account_address: {_eq: "0x59e9982aaa5194058e51c8be75519ec54c518ea3651b3a4a87c39b9bd88ba314"}, user_transaction: {entry_function_id_str: {_like: "0x6e042405369c44a5fec8ddd28bb47442cf0256367b74b9aea64ae0e6ed90b489::market%"}}}
+        ) {
+            aggregate {
+            count
+            }
+        }
+		event_market_interactions: account_transactions_aggregate(
+            where: {account_address: {_eq: "0x59e9982aaa5194058e51c8be75519ec54c518ea3651b3a4a87c39b9bd88ba314"}, user_transaction: {entry_function_id_str: {_like: "0x6e042405369c44a5fec8ddd28bb47442cf0256367b74b9aea64ae0e6ed90b489::event_market%"}}}
         ) {
             aggregate {
             count
             }
         }
         total_votes: account_transactions_aggregate(
-            where: {account_address: {_eq: "${sender}"}, user_transaction: {entry_function_id_str: {_eq: "${moduleAddress}::market::vote"}}}
+            where: {account_address: {_eq: "0x59e9982aaa5194058e51c8be75519ec54c518ea3651b3a4a87c39b9bd88ba314"}, user_transaction: {entry_function_id_str: {_in: ["0x6e042405369c44a5fec8ddd28bb47442cf0256367b74b9aea64ae0e6ed90b489::market::vote", "0x6e042405369c44a5fec8ddd28bb47442cf0256367b74b9aea64ae0e6ed90b489::event_market::vote"]}}}
         ) {
             aggregate {
             count
             }
         }
         placed_bets: coin_activities_aggregate(
-            where: {owner_address: {_eq: "${sender}"}, entry_function_id_str: {_eq: "${moduleAddress}::market::place_bet"}, activity_type: {_eq: "0x1::coin::WithdrawEvent"}}
+            where: {owner_address: {_eq: "0x59e9982aaa5194058e51c8be75519ec54c518ea3651b3a4a87c39b9bd88ba314"}, entry_function_id_str: {_in: ["0x6e042405369c44a5fec8ddd28bb47442cf0256367b74b9aea64ae0e6ed90b489::market::place_bet", "0x6e042405369c44a5fec8ddd28bb47442cf0256367b74b9aea64ae0e6ed90b489::event_market::place_bet"]}, activity_type: {_eq: "0x1::coin::WithdrawEvent"}}
         ) {
             aggregate {
                 count
             }
         }
         placed_bets: coin_activities_aggregate(
-            where: {owner_address: {_eq: "${sender}"}, entry_function_id_str: {_eq: "${moduleAddress}::market::place_bet"}, activity_type: {_eq: "0x1::coin::WithdrawEvent"}}
+            where: {owner_address: {_eq: "0x59e9982aaa5194058e51c8be75519ec54c518ea3651b3a4a87c39b9bd88ba314"}, entry_function_id_str: {_in: ["0x6e042405369c44a5fec8ddd28bb47442cf0256367b74b9aea64ae0e6ed90b489::market::place_bet", "0x6e042405369c44a5fec8ddd28bb47442cf0256367b74b9aea64ae0e6ed90b489::event_market::place_bet"]}, activity_type: {_eq: "0x1::coin::WithdrawEvent"}}
         ) {
             aggregate {
                 sum {
@@ -40,61 +47,61 @@ export const ProfilePageStatsQuery = (sender: Address, moduleAddress: Address) =
             }
         }
         apt_placed:events(
-            where: {indexed_type: {_eq: "${moduleAddress}::market::PlaceBet<${moduleAddress}::switchboard_asset::APT>"}, data: {_cast: {String: {_like: "%${sender}%"}}}}
+            where: {indexed_type: {_eq: "0x6e042405369c44a5fec8ddd28bb47442cf0256367b74b9aea64ae0e6ed90b489::market::PlaceBet<0x6e042405369c44a5fec8ddd28bb47442cf0256367b74b9aea64ae0e6ed90b489::switchboard_asset::APT>"}, data: {_cast: {String: {_like: "%0x59e9982aaa5194058e51c8be75519ec54c518ea3651b3a4a87c39b9bd88ba314%"}}}}
             order_by: {transaction_block_height: desc}
         ) {
             data
         }
         btc_placed:events(
-            where: {indexed_type: {_eq: "${moduleAddress}::market::PlaceBet<${moduleAddress}::switchboard_asset::BTC>"}, data: {_cast: {String: {_like: "%${sender}%"}}}}
+            where: {indexed_type: {_eq: "0x6e042405369c44a5fec8ddd28bb47442cf0256367b74b9aea64ae0e6ed90b489::market::PlaceBet<0x6e042405369c44a5fec8ddd28bb47442cf0256367b74b9aea64ae0e6ed90b489::switchboard_asset::BTC>"}, data: {_cast: {String: {_like: "%0x59e9982aaa5194058e51c8be75519ec54c518ea3651b3a4a87c39b9bd88ba314%"}}}}
             order_by: {transaction_block_height: desc}
         ) {
             data
         }
         eth_placed:events(
-            where: {indexed_type: {_eq: "${moduleAddress}::market::PlaceBet<${moduleAddress}::switchboard_asset::ETH>"}, data: {_cast: {String: {_like: "%${sender}%"}}}}
+            where: {indexed_type: {_eq: "0x6e042405369c44a5fec8ddd28bb47442cf0256367b74b9aea64ae0e6ed90b489::market::PlaceBet<0x6e042405369c44a5fec8ddd28bb47442cf0256367b74b9aea64ae0e6ed90b489::switchboard_asset::ETH>"}, data: {_cast: {String: {_like: "%0x59e9982aaa5194058e51c8be75519ec54c518ea3651b3a4a87c39b9bd88ba314%"}}}}
             order_by: {transaction_block_height: desc}
         ) {
             data
         }
         sol_placed:events(
-            where: {indexed_type: {_eq: "${moduleAddress}::market::PlaceBet<${moduleAddress}::switchboard_asset::SOL>"}, data: {_cast: {String: {_like: "%${sender}%"}}}}
+            where: {indexed_type: {_eq: "0x6e042405369c44a5fec8ddd28bb47442cf0256367b74b9aea64ae0e6ed90b489::market::PlaceBet<0x6e042405369c44a5fec8ddd28bb47442cf0256367b74b9aea64ae0e6ed90b489::switchboard_asset::SOL>"}, data: {_cast: {String: {_like: "%0x59e9982aaa5194058e51c8be75519ec54c518ea3651b3a4a87c39b9bd88ba314%"}}}}
             order_by: {transaction_block_height: desc}
         ) {
             data
         }
         usdc_placed:events(
-            where: {indexed_type: {_eq: "${moduleAddress}::market::PlaceBet<${moduleAddress}::switchboard_asset::USDC>"}, data: {_cast: {String: {_like: "%${sender}%"}}}}
+            where: {indexed_type: {_eq: "0x6e042405369c44a5fec8ddd28bb47442cf0256367b74b9aea64ae0e6ed90b489::market::PlaceBet<0x6e042405369c44a5fec8ddd28bb47442cf0256367b74b9aea64ae0e6ed90b489::switchboard_asset::USDC>"}, data: {_cast: {String: {_like: "%0x59e9982aaa5194058e51c8be75519ec54c518ea3651b3a4a87c39b9bd88ba314%"}}}}
             order_by: {transaction_block_height: desc}
         ) {
             data
         }
-            apt:events(
-            where: {indexed_type: {_eq: "${moduleAddress}::market::CreateMarket<${moduleAddress}::switchboard_asset::APT>"}, data: {_cast: {String: {_like: "%${sender}%"}}}}
+        apt:events(
+            where: {indexed_type: {_eq: "0x6e042405369c44a5fec8ddd28bb47442cf0256367b74b9aea64ae0e6ed90b489::market::CreateMarket<0x6e042405369c44a5fec8ddd28bb47442cf0256367b74b9aea64ae0e6ed90b489::switchboard_asset::APT>"}, data: {_cast: {String: {_like: "%0x59e9982aaa5194058e51c8be75519ec54c518ea3651b3a4a87c39b9bd88ba314%"}}}}
             order_by: {transaction_block_height: desc}
         ) {
             account_address
         }
         btc:events(
-            where: {indexed_type: {_eq: "${moduleAddress}::market::CreateMarket<${moduleAddress}::switchboard_asset::BTC>"}, data: {_cast: {String: {_like: "%${sender}%"}}}}
+            where: {indexed_type: {_eq: "0x6e042405369c44a5fec8ddd28bb47442cf0256367b74b9aea64ae0e6ed90b489::market::CreateMarket<0x6e042405369c44a5fec8ddd28bb47442cf0256367b74b9aea64ae0e6ed90b489::switchboard_asset::BTC>"}, data: {_cast: {String: {_like: "%0x59e9982aaa5194058e51c8be75519ec54c518ea3651b3a4a87c39b9bd88ba314%"}}}}
             order_by: {transaction_block_height: desc}
         ) {
             account_address
         }
         eth:events(
-            where: {indexed_type: {_eq: "${moduleAddress}::market::CreateMarket<${moduleAddress}::switchboard_asset::ETH>"}, data: {_cast: {String: {_like: "%${sender}%"}}}}
+            where: {indexed_type: {_eq: "0x6e042405369c44a5fec8ddd28bb47442cf0256367b74b9aea64ae0e6ed90b489::market::CreateMarket<0x6e042405369c44a5fec8ddd28bb47442cf0256367b74b9aea64ae0e6ed90b489::switchboard_asset::ETH>"}, data: {_cast: {String: {_like: "%0x59e9982aaa5194058e51c8be75519ec54c518ea3651b3a4a87c39b9bd88ba314%"}}}}
             order_by: {transaction_block_height: desc}
         ) {
             account_address
         }
         sol:events(
-            where: {indexed_type: {_eq: "${moduleAddress}::market::CreateMarket<${moduleAddress}::switchboard_asset::SOL>"}, data: {_cast: {String: {_like: "%${sender}%"}}}}
+            where: {indexed_type: {_eq: "0x6e042405369c44a5fec8ddd28bb47442cf0256367b74b9aea64ae0e6ed90b489::market::CreateMarket<0x6e042405369c44a5fec8ddd28bb47442cf0256367b74b9aea64ae0e6ed90b489::switchboard_asset::SOL>"}, data: {_cast: {String: {_like: "%0x59e9982aaa5194058e51c8be75519ec54c518ea3651b3a4a87c39b9bd88ba314%"}}}}
             order_by: {transaction_block_height: desc}
         ) {
             account_address
         }
         usdc:events(
-            where: {indexed_type: {_eq: "${moduleAddress}::market::CreateMarket<${moduleAddress}::switchboard_asset::USDC>"}, data: {_cast: {String: {_like: "%${sender}%"}}}}
+            where: {indexed_type: {_eq: "0x6e042405369c44a5fec8ddd28bb47442cf0256367b74b9aea64ae0e6ed90b489::market::CreateMarket<0x6e042405369c44a5fec8ddd28bb47442cf0256367b74b9aea64ae0e6ed90b489::switchboard_asset::USDC>"}, data: {_cast: {String: {_like: "%0x59e9982aaa5194058e51c8be75519ec54c518ea3651b3a4a87c39b9bd88ba314%"}}}}
             order_by: {transaction_block_height: desc}
         ) {
             account_address
